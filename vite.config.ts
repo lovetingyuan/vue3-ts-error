6207acd2c5a63b363b2c49244cde9325bfcda133
tingyuan <hbtwhucsis@gmail.com>
2021-02-24 22:16:52 +0800

    chore

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite'
-import type { Plugin } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import { readFileSync } from 'fs'
@@ -32,5 +31,5 @@ export default defineConfig({
         }
       }
     }
-  } as Plugin]
+  }]
 })
